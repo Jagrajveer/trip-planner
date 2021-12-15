4cadfe4f6a7a2d132821607e20300c6feccf0b2c
jagrajveer <jagrajveer9@gmail.com>
2021-12-15 17:14:50 -0600

    add getDestinationPlacesList function

--- src/script/app.js
+++ src/script/app.js
@@ -8,6 +8,10 @@ const map_box_base_url = "https://api.mapbox.com/geocoding/v5/mapbox.places";
 // elements
 const OriginFormElement = document.querySelector(".origin-form");
 const OriginInputElement = document.querySelector(".origin-form input");
+const DestinationFormElement = document.querySelector(".destination-form");
+const DestinationInputElement = document.querySelector(
+  ".destination-form input"
+);
 
 // using getCurrentPosition get user's coordinates and pass it to the getPlaces
 const getOriginPlacesList = (e) => {
@@ -28,6 +32,23 @@ const getOriginPlacesList = (e) => {
   }
 };
 
+// using getCurrentPosition get user's coordinates and pass it to the getPlaces
+const getDestinationPlacesList = (e) => {
+  e.preventDefault();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        getPlaces(latitude, longitude, DestinationInputElement.value).then(
+          (data) => console.log(data)
+        );
+      }
+    );
+  } else {
+    document.getElementsByTagName("body")[0].innerHTML =
+      "Geolocation is not supported in this browser.";
+  }
+};
+
 // this function uses mapbox api to get list of places using the given keywords
 const getPlaces = async (latitude, longitude, place) => {
   const url = `${map_box_base_url}/${place}.json?bbox=-97.32972, 49.762789, -96.951241, 49.994007&proximity=${longitude}, ${latitude}&access_token=${MAP_BOX_API_KEY}`;
@@ -37,3 +58,4 @@ const getPlaces = async (latitude, longitude, place) => {
 };
 
 OriginFormElement.addEventListener("submit", getOriginPlacesList);
+DestinationFormElement.addEventListener("submit", getDestinationPlacesList);
