c8ae4b067ae1bf313b1fb602c60bb7585664d205
jagrajveer <jagrajveer9@gmail.com>
2021-12-18 23:28:23 -0600

    add conditions to take care of edge cases

--- src/script/planMyTrip.js
+++ src/script/planMyTrip.js
@@ -10,54 +10,59 @@ export const renderRoute = (data) => {
     "<li><h3>Recommended trip</h3></li>"
   );
 
-  plans[0].segments.map((segment) => {
-    let step = "";
+  if (plans.length > 0) {
+    plans[0].segments.map((segment) => {
+      let step = "";
 
-    if (segment.type === "walk") {
-      if (segment.to.stop) {
-        step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to ${segment.to.stop.key} - ${segment.to.stop.name}.</li>`;
-      } else if (segment.to.destination) {
-        step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to your destination.</li>`;
+      if (segment.type === "walk") {
+        if (segment.to.stop) {
+          step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to ${segment.to.stop.key} - ${segment.to.stop.name}.</li>`;
+        } else if (segment.to.destination) {
+          step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to your destination.</li>`;
+        }
       }
-    }
-    if (segment.type === "ride") {
-      if (segment.route.key === "BLUE") {
-        step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.key} for ${segment.times.durations.riding} minutes.</li>`;
-      } else {
-        step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.name} for ${segment.times.durations.riding} minutes.</li>`;
+      if (segment.type === "ride") {
+        if (segment.route.key === "BLUE") {
+          step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.key} for ${segment.times.durations.riding} minutes.</li>`;
+        } else {
+          step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.name} for ${segment.times.durations.riding} minutes.</li>`;
+        }
       }
-    }
-    if (segment.type === "transfer") {
-      step = `<li><i class="fas fa-ticket-alt"></i>transfer from stop ${segment.from.stop.name} to stop ${segment.to.stop.name}.</li>`;
-    }
+      if (segment.type === "transfer") {
+        step = `<li><i class="fas fa-ticket-alt"></i>transfer from stop ${segment.from.stop.name} to stop ${segment.to.stop.name}.</li>`;
+      }
 
-    RouteElement.insertAdjacentHTML("beforeend", step);
-  });
-  RouteElement.insertAdjacentHTML(
-    "beforeend",
-    "<li><h3>Alternative trips</h3></li>"
-  );
-  plans[1].segments.map((segment) => {
-    let step = "";
+      RouteElement.insertAdjacentHTML("beforeend", step);
+    });
+    RouteElement.insertAdjacentHTML(
+      "beforeend",
+      "<li><h3>Alternative trips</h3></li>"
+    );
 
-    if (segment.type === "walk") {
-      if (segment.to.stop) {
-        step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to ${segment.to.stop.key} - ${segment.to.stop.name}.</li>`;
-      } else if (segment.to.destination) {
-        step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to your destination.</li>`;
-      }
-    }
-    if (segment.type === "ride") {
-      if (segment.route.key === "BLUE") {
-        step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.key} for ${segment.times.durations.riding} minutes.</li>`;
-      } else {
-        step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.name} for ${segment.times.durations.riding} minutes.</li>`;
-      }
-    }
-    if (segment.type === "transfer") {
-      step = `<li><i class="fas fa-ticket-alt"></i>transfer from stop ${segment.from.stop.name} to stop ${segment.to.stop.name}.</li>`;
-    }
+    if (plans.length > 1) {
+      plans[1].segments.map((segment) => {
+        let step = "";
 
-    RouteElement.insertAdjacentHTML("beforeend", step);
-  });
+        if (segment.type === "walk") {
+          if (segment.to.stop) {
+            step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to ${segment.to.stop.key} - ${segment.to.stop.name}.</li>`;
+          } else if (segment.to.destination) {
+            step = `<li><i class="fas fa-walking"></i>Walk for ${segment.times.durations.walking} minutes to your destination.</li>`;
+          }
+        }
+        if (segment.type === "ride") {
+          if (segment.route.key === "BLUE") {
+            step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.key} for ${segment.times.durations.riding} minutes.</li>`;
+          } else {
+            step = `<li><i class="fas fa-bus" ></i>Ride the ${segment.route.name} for ${segment.times.durations.riding} minutes.</li>`;
+          }
+        }
+        if (segment.type === "transfer") {
+          step = `<li><i class="fas fa-ticket-alt"></i>transfer from stop ${segment.from.stop.name} to stop ${segment.to.stop.name}.</li>`;
+        }
+
+        RouteElement.insertAdjacentHTML("beforeend", step);
+      });
+    }
+  }
 };
