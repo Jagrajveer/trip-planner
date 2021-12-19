0763757247bfa9a4be001b1fc0eb0a8f395049a6
jagrajveer <jagrajveer9@gmail.com>
2021-12-19 14:52:16 -0600

    add gulp tasks to create production files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
 const { src, dest, series } = require("gulp");
 const cleanCSS = require("gulp-clean-css");
-const minify = require("gulp-uglify");
 const sourcemaps = require("gulp-sourcemaps");
+const gulpWebpack = require("webpack-stream");
 
 const moveHTML = () => {
   return src("src/*.html").pipe(dest("dist/"));
 };
 
-exports.default = series(moveHTML);
+const moveCSS = () => {
+  return src("src/style/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist/style/"));
+};
+
+const moveJS = () => {
+  return src("src/script/*.js")
+    .pipe(gulpWebpack({ mode: "production", output: { filename: "app.js" } }))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist/script/"));
+};
+
+exports.default = series(moveHTML, moveCSS, moveJS);
 exports.html = moveHTML;
+exports.css = moveCSS;
+exports.js = moveJS;
